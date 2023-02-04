e4c28cb913c0cf9012e237b3301d8ed22cbbdc81
Syed Al Mohaimin <120173044+AlMohaimin037@users.noreply.github.com>
2023-02-04 10:43:30 +0600

    Add files via upload

---
+++ Basic Introductory Problems/12.Program that will define a constant using CONST and print the value/main12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//Program that will define a constant using “CONST” and print the value.
+
+int main()
+{
+    float const pi = 3.14, goldenRatio = 1.62;
+
+    printf("The value of pi: %.2f\nThe value of golden ratio: %.2f\n", pi, goldenRatio);
+
+    return 0;
+
+}
