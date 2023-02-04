0267a08d0e4dc7841846e7ec2cc63d5bb1a4f79c
Syed Al Mohaimin <120173044+AlMohaimin037@users.noreply.github.com>
2023-02-04 10:49:07 +0600

    Add files via upload

---
+++ Basic Introductory Problems/13.Program that will define a constant using DEFINE and print the value/main13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define height 200
+#define pi 3.14
+
+//Program that will define a constant using “DEFINE” and print the value
+
+int main()
+{
+    printf("The value of HEIGHT: %d\nThe value of PI: %.2f\n", height, pi);
+    return 0;
+}
