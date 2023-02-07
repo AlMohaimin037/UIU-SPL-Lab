bd1fd2eabf9b0deed984879bddd837826605e313
Syed Al Mohaimin <120173044+AlMohaimin037@users.noreply.github.com>
2023-02-07 13:01:08 +0600

    Update main15.c

--- Basic Introductory Problems/15.Program that will take an floating number and print the number right justified within 10 columns 2 columns rounded to two decimal places rounded to int expo notation/main15.c
+++ Basic Introductory Problems/15.Program that will take an floating number and print the number right justified within 10 columns 2 columns rounded to two decimal places rounded to int expo notation/main15.c
@@ -3,7 +3,7 @@
 
 /*Program that will take an floating point number as input from the keyboard and use printf
 function to perform the followings:
-(a) Print the number right justified within 10 columns
+(a) Print the number right justified after 10 columns
 (b) Print the number to be right justified to 2 columns (Assuming the input has more
 than 2 digits)
 (c) Print the number rounded to two decimal places
@@ -17,7 +17,7 @@ int main()
     scanf("%f", &num1);
 
 
-    printf("Val:%10f\nVal:%2f\nVal:%.2f\nVal:%.0f\nVal:%e\n", num1, num1, num1, floor(num1), num1);//%.0f for floor format specifier.%d will give 0.
+    printf("Val:  \t%f\nVal:%2f\nVal:%.2f\nVal:%.0f\nVal:%e\n", num1, num1, num1, floor(num1), num1);//%.0f for floor format specifier.%d will give 0.
 
     return 0;
 }
