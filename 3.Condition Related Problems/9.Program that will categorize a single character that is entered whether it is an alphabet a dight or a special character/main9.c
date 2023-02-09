240a21af977aca2108b41b6324041c006b81c2bf
Syed Al Mohaimin <120173044+AlMohaimin037@users.noreply.github.com>
2023-02-09 17:19:22 +0600

    Add files via upload

---
+++ 3.Condition Related Problems/9.Program that will categorize a single character that is entered whether it is an alphabet a dight or a special character/main9.c
@@ -0,0 +1,18 @@
+/*Program that will categorize a single character that is entered at the terminal, whether it is an alphabet, a digit or a special character.
+
+(Restriction: Without math.h)
+*/
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        printf("Alphabet\n");
+    else if(ch >= '0' && ch <= '9')
+        printf("Digit\n");
+    else
+        printf("Special\n");
+    return 0;
+}
