737c2b5252015d238d695507c2124fae8f04a2e1
Syed Al Mohaimin <120173044+AlMohaimin037@users.noreply.github.com>
2023-04-14 21:04:44 +0600

    Create problem02_01.c

---
+++ Assignment01/problem02_01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define SIZE 100
+
+void printArr(int * arr, int size);
+
+int main()
+{
+    int arr[SIZE], n;
+    int * left = arr;
+    printf("Enter size of an array: ");
+    scanf("%d", &n);
+    
+    int * right = &arr[n-1];
+    
+    printf("Enter %d integers: ", n);
+    while(left <= right)
+    {
+        scanf("%d", left);
+        left++;
+    }
+    printArr(arr, n);
+    
+    return 0;
+}
+
+void printArr(int * arr, int size)
+{
+    int * arrEnd = (arr + size - 1);
+    
+    printf("The elements are: \n");
+    while(arr <= arrEnd)
+    {
+        printf("%d ", *arr);
+        arr++;
+    }
+}
